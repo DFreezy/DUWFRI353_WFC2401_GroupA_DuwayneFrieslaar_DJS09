68b8903318b4979382726faf21047edf03bb9e8c
Duwayne Frieslaar <155962320+DFreezy@users.noreply.github.com>
2024-06-09 00:29:14 +0200

    Added a function
    Write a function that will only accept numbers

--- index.ts
+++ index.ts
@@ -24,3 +24,10 @@ const reviews = [
         date: '27-03-2021'
     },
 ]
+
+// Solution
+function showReviewTotal (value : number) {
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString()
+}
+
+showReviewTotal(reviews.length)
