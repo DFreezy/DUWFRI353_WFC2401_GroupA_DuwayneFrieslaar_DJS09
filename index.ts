a87f9e68a86598bf4ed6a7a6676e5d02d59fcb24
Duwayne Frieslaar <155962320+DFreezy@users.noreply.github.com>
2024-06-09 08:10:36 +0200

    Displayed most recent viewers
    I displayed the most recent viewers from the array

--- index.ts
+++ index.ts
@@ -1,7 +1,3 @@
-// Number Types mini-challenge 10 10.2
-// Write a function that will only accept numbers and attend to 
-// all TypeScript weakness flags.
-// : number
 const reviewTotalDisplay = document.querySelector('#reviews')
 
 const reviews = [
@@ -26,8 +22,8 @@ const reviews = [
 ]
 
 // Solution
-function showReviewTotal (value : number) {
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString()
+function showReviewTotal (value : number, reviewer: string) {
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer
 }
 
-showReviewTotal(reviews.length)
+showReviewTotal(reviews.length, reviews[0].name)
