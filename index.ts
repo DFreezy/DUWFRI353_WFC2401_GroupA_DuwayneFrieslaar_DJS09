b89cbfe19b505fcb59c5b18d72e0f1069d829840
Duwayne Frieslaar <155962320+DFreezy@users.noreply.github.com>
2024-06-09 18:04:54 +0200

    added review object
    I added the review object

--- index.ts
+++ index.ts
@@ -32,6 +32,31 @@ function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
 
+const reviews : {
+    name: string;
+    stars: number;
+    loyaltyUser: boolean;
+    date: string;
+}[] = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
 
 
 const you: {
@@ -39,12 +64,14 @@ const you: {
     lastName: string
     isReturning: boolean;
     age: number;
+    stayedAt: string[];
 
 } = {
    firstName: 'Bobby',
    lastName: 'Brown',
    isReturning: true,
    age: 35,
+   stayedAt: ['hamptons beach', 'washington complex']
 }
 
 
