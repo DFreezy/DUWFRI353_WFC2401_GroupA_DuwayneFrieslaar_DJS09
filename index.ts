53a180caf9a13facd6506f2feb64d14794671721
Duwayne Frieslaar <155962320+DFreezy@users.noreply.github.com>
2024-06-09 08:50:26 +0200

    Welcomed user
    Add welcome user in case it's a users first visit. Add welcome back if user is returning

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
 
 const reviews = [
@@ -21,11 +23,29 @@ const reviews = [
     },
 ]
 
-// Solution
-function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean) {
+
+function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
     const iconDisplay = isLoyalty ? '⭐' : ''
     reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+
+
+
+const you = {
+    userName: 'Bobby',
+    isReturning: true,
+}
+
+
+function populateUser(isReturning : boolean, userName : string ) {
+    if (isReturning){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName)
 
